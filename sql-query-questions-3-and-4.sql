23ddc0badb049d93473d46617dca8c8219b0046d
Brian Miller <68014965+brian-millerj@users.noreply.github.com>
2024-06-18 11:06:06 -0400

    SQL query
    SQL query to identify questions 3 and 4 from take home assessment. Output can be found in README write up.

---
+++ sql-query-questions-3-and-4.sql
@@ -0,0 +1,12 @@
+--Question 3: When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+--Question 4: When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+SELECT
+  rewardsReceiptStatus as receiptStatus,
+  ROUND(AVG(totalSpent),2) AS Average_totalSpent,
+  SUM(purchasedItemCount) AS Total_purchasedItemCount
+FROM
+  FetchRewards.dbo.receipts
+WHERE
+  rewardsReceiptStatus in ('FINISHED', 'REJECTED')
+GROUP BY
+  rewardsReceiptStatus;
